563a629466753552ce8753b63800fa4f7645a2ce
fedellen <fedellen@fedellen-ubuntu.lan>
2020-12-03 14:28:08 -0600

    Add createList resolver

--- src/resolvers/list.ts
+++ src/resolvers/list.ts
@@ -1,4 +1,4 @@
-import { List } from '../entities';
+import { List, UserToList } from '../entities';
 import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 import { getRepository } from 'typeorm';
 
@@ -6,8 +6,23 @@ import { getRepository } from 'typeorm';
 export class ListResolver {
   // Retrieves ALL lists, refactor later to retrieve only user's list
   @Query(() => [List])
-  lists(): Promise<List[]> {
+  getAllLists(): Promise<List[]> {
     const listRepository = getRepository(List);
     return listRepository.find({});
+  }
+
+  @Mutation(() => List)
+  async createList(
+    @Arg('title') title: string,
+    @Arg('userId') userId: string
+  ): Promise<List> {
+    const list = await List.create({
+      title
+    }).save();
+    await UserToList.create({
+      listId: list.id,
+      userId
+    }).save();
+    return list;
   }
 }
