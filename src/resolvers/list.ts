55b1cdbca7e6a16c9205a65afe51d4f76e172c7e
fedellen <fedellen@fedellen-ubuntu.lan>
2020-12-08 22:11:34 -0600

    Add context to list resolver

--- src/resolvers/list.ts
+++ src/resolvers/list.ts
@@ -1,17 +1,25 @@
 import { List, User, UserToList } from '../entities';
-import { Arg, Mutation, Query, Resolver, UseMiddleware } from 'type-graphql';
+import {
+  Arg,
+  Ctx,
+  Mutation,
+  Query,
+  Resolver,
+  UseMiddleware
+} from 'type-graphql';
 import { isAuth } from '../middleware/isAuth';
 import { logger } from '../middleware/logger';
 import { ShareListInput } from './input-types/ShareListInput';
+import { MyContext } from 'src/types/MyContext';
 
 @Resolver()
 export class ListResolver {
   // Gets only the specified user's lists
   @UseMiddleware(isAuth, logger)
   @Query(() => [UserToList])
-  async getUsersLists(@Arg('userId') userId: string): Promise<UserToList[]> {
+  async getUsersLists(@Ctx() { req }: MyContext): Promise<UserToList[]> {
     const usersListArray = UserToList.find({
-      where: { userId: userId },
+      where: { userId: req.session.userId },
       relations: ['list', 'list.items', 'itemHistory']
     });
     if (!usersListArray) throw new Error('No lists were found for that User..');
@@ -24,14 +32,14 @@ export class ListResolver {
   @Mutation(() => List)
   async createList(
     @Arg('title') title: string,
-    @Arg('userId') userId: string
+    @Ctx() { req }: MyContext
   ): Promise<List> {
     const list = await List.create({
       title
     }).save();
     await UserToList.create({
       listId: list.id,
-      userId
+      userId: req.session.userId
     }).save();
     return list;
   }
@@ -40,8 +48,19 @@ export class ListResolver {
   @UseMiddleware(isAuth, logger)
   @Mutation(() => Boolean)
   async shareList(
-    @Arg('data') { email, listId, privileges }: ShareListInput
+    @Arg('data') { email, listId, privileges }: ShareListInput,
+    @Ctx() { req }: MyContext
   ): Promise<Boolean> {
+    const userId = req.session.userId;
+    const userOwnerOfList = await UserToList.findOne({
+      where: { listId: listId, userId: userId }
+    });
+
+    if (!userOwnerOfList)
+      throw new Error('You do not have access to that list..');
+    if (userOwnerOfList.privileges !== ['owner'])
+      throw new Error('You do not have owner privileges to that list..');
+
     const user = await User.findOne({ where: { email } });
     if (!user) {
       throw new Error(
