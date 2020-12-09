7539a8c965ed3ce1c234de80279ee11e30ab0d0e
fedellen <fedellen@fedellen-ubuntu.lan>
2020-12-09 12:07:39 -0600

    Add userWithList test helper function

--- src/test/helpers/createUser.ts
+++ src/test/helpers/createUser.ts
@@ -1,4 +1,4 @@
-import { User } from '../../entities';
+import { List, User, UserToList } from '../../entities';
 import faker from 'faker';
 import argon2 from 'argon2';
 
@@ -20,4 +20,17 @@ export const createUser = async (confirmed: boolean = true): Promise<User> => {
   savedUser.password = user.password;
 
   return savedUser;
+};
+
+export const userWithList = async (): Promise<User> => {
+  const user = await createUser();
+  const list = await List.create({
+    title: 'my-test-list'
+  }).save();
+  await UserToList.create({
+    listId: list.id,
+    userId: user.id
+  }).save();
+
+  return user;
 };
