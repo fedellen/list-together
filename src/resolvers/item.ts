b55a95448bb34b9b18abb7aeb47b1978dcf40187
fedellen <fedellen@fedellen-ubuntu.lan>
2020-12-10 12:31:22 -0600

    Add user auth, privilege auth, and item limit to addItem

--- src/resolvers/item.ts
+++ src/resolvers/item.ts
@@ -1,28 +1,45 @@
 import { Item, ItemHistory, UserToList } from '../entities';
-import { Arg, Mutation, Resolver } from 'type-graphql';
+import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from 'type-graphql';
+import { isAuth } from '../middleware/isAuth';
+import { logger } from '../middleware/logger';
+import { MyContext } from '../types/MyContext';
 
 @Resolver()
 export class ItemResolver {
   // Send single item only when connection already exists from front
-  // Otherwise items will be sent in arrays to save on requests
+  // Otherwise list state should be sent as one object
+  @UseMiddleware(isAuth, logger)
   @Mutation(() => UserToList)
   async addItem(
     @Arg('name') nameInput: string,
     @Arg('listId') listId: string,
-    @Arg('userId') userId: string
+    @Ctx() { req }: MyContext
   ): Promise<UserToList> {
-    // find the list
+    const userId = req.session.userId;
+    console.log('here is the userId from item resolver: ', userId);
     const userToListTable = await UserToList.findOne({
       where: { listId: listId, userId: userId },
       relations: ['list', 'list.items', 'itemHistory']
     });
-    if (!userToListTable)
+
+    if (!userToListTable) {
       throw new Error('Could not find that user to list connection..');
+    } else if (
+      !userToListTable.privileges.includes('owner') &&
+      !userToListTable.privileges.includes('add')
+    ) {
+      throw new Error(
+        'User does not have privileges to add items to that list..'
+      );
+    }
 
     const list = userToListTable.list;
     if (list.items) {
       // Handle itemExists condition on the front end
       // This provides security for synchronization conflicts
+      if (list.items.length >= 300)
+        throw new Error('Lists cannot have more than 300 items..');
+
       const itemExists = list.items.find(({ name }) => name === nameInput);
       if (itemExists) throw new Error('Item already exists on this list..');
 
@@ -56,4 +73,8 @@ export class ItemResolver {
   }
 
   // Delete item from list
+
+  // Style item on list
+
+  // Add note to item on list
 }
