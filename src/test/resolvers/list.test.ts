721b254d3ce27ec20c13a3991f8ffccdc6c8a156
fedellen <fedellen@fedellen-ubuntu.lan>
2020-12-09 20:10:36 -0600

    Add tests for list limit and unauthorized access

--- src/test/resolvers/list.test.ts
+++ src/test/resolvers/list.test.ts
@@ -81,7 +81,7 @@ describe('Get users lists query:', () => {
         getUsersLists: [
           {
             list: {
-              title: 'my-test-list'
+              title: 'my-test-list-0'
             }
           }
         ]
@@ -132,9 +132,45 @@ describe('Create list mutation:', () => {
     expect(listConnectionsInDatabase).toHaveLength(1);
   });
 
-  it('User cannot create more than 25 lists as owner', () => {});
+  it('User cannot create more than 25 lists as owner', async () => {
+    const user = await userWithList(25); // creates user with 25 lists
+    const title = faker.name.title();
 
-  it('Users without authenticated context cannot create lists', () => {});
+    const response = await graphqlCall({
+      source: createListMutation,
+      variableValues: { title: title },
+      userId: user.id
+    });
+
+    expect(response).toMatchObject({
+      errors: [
+        {
+          message: 'User cannot create more than 25 lists..'
+        }
+      ]
+    });
+
+    const listConnectionsInDatabase = await UserToList.find({
+      where: { userId: user.id }
+    });
+
+    expect(listConnectionsInDatabase).toHaveLength(25);
+  });
+
+  it('Unauthorized response with no userId in context', async () => {
+    const response = await graphqlCall({
+      source: createListMutation,
+      variableValues: { title: faker.name.title() }
+    });
+
+    expect(response).toMatchObject({
+      errors: [
+        {
+          message: 'Not authenticated..'
+        }
+      ]
+    });
+  });
 });
 
 // describe('Share list mutation:', () => {
@@ -153,4 +189,18 @@ describe('Create list mutation:', () => {
 //   it('Owner of list can rename the list', () => {});
 
 //   it('Non-owners cannot rename the list', () => {});
+// });
+
+// describe('Sort list mutation:', () => {
+//   it('User can sort the order of in which their lists are displayed', () => {});
+
+//   it('Non-owners cannot rename the list', () => {});
+// });
+
+// describe('Re-order list mutation:', () => {
+//   it('User can submit a re-ordered list', () => {});
+
+//   it('User can automatically re-order the list', () => {});
+
+//   it('When user recieves a list with new items, the new items are presented at the top of their Sorted List', () => {});
 // });
