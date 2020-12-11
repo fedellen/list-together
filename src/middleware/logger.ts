afceb8f5e83d749a55cd6512395253bc31de1a09
fedellen <fedellen@fedellen-ubuntu.lan>
2020-12-10 20:17:39 -0600

    Modify logger to include useful debugging info

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -2,8 +2,16 @@ import { MiddlewareFn } from 'type-graphql';
 
 import { MyContext } from '../types/MyContext';
 
-export const logger: MiddlewareFn<MyContext> = async ({ args }, next) => {
-  console.log('args: ', args);
+export const logger: MiddlewareFn<MyContext> = async (
+  { args, context, info },
+  next
+) => {
+  console.log(
+    `🔥 New GraphQL Request! 🔥 
+    Arguments: ${JSON.stringify(args)},
+    UserId: ${context.req.session.userId},
+    ResolverType: ${info.fieldName}`
+  );
 
   return next();
 };
