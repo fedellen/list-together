48b1085a036b3d5b753ccbd90693ed2a6dc40343
fedellen <fedellen@pixelpajamastudios.com>
2020-12-28 15:56:15 -0600

    Initialize side menu

---
+++ pwa/src/components/SideMenu.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { useAddItemMutation } from 'src/generated/graphql';
+import { SingleInput } from './SingleInput';
+import { AddItemIcon } from './svg/AddItemIcon';
+import { SideMenuStates } from './UsersLists';
+
+type SideMenuProps = {
+  state: SideMenuStates;
+  setState: (arg: SideMenuStates) => void;
+  currentListId: string;
+};
+
+export function SideMenu({ state, setState, currentListId }: SideMenuProps) {
+  const [addItem, { loading }] = useAddItemMutation();
+  const [toggleInput, setToggleInput] = useState(false);
+
+  const handleAddItem = async (item: string) => {
+    if (!loading) {
+      if (state !== 'add') setState('add');
+      setToggleInput(!toggleInput);
+      try {
+        await addItem({
+          variables: {
+            data: {
+              nameInput: item,
+              listId: currentListId
+            }
+          }
+          // update: (cache, {data}) => {
+          //   cache.writeQuery({
+          //     query: getUs
+          //   })
+          // }
+        });
+      } catch (err) {
+        console.error('Error on Add Item mutation: ', err);
+      }
+    }
+  };
+
+  // const toggleAddItemInput =
+
+  return (
+    <div className="absolute right-6 bottom-6">
+      {toggleInput && (
+        <SingleInput
+          purpose="addItem"
+          handleAdd={handleAddItem}
+          handleExit={() => setToggleInput(!toggleInput)}
+        />
+      )}
+      <AddItemIcon toggleAddItem={() => setToggleInput(!toggleInput)} />
+    </div>
+  );
+}
