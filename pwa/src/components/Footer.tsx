ca8b9f3740d0fdacc0592fc1cbc1fdc5c0cc2488
fedellen <fedellen@pixelpajamastudios.com>
2020-12-28 15:56:01 -0600

    Change tailwind styles

--- pwa/src/components/Footer.tsx
+++ pwa/src/components/Footer.tsx
@@ -1,3 +1,12 @@
 export function Footer() {
-  return <div className="h-28 bg-darker"></div>;
+  return (
+    <div className="h-28 bg-darker border-light border-t-4  grid grid-cols-3 gap-2 justify-items-center items-center  border-b-4">
+      <div></div>
+      <div className="text-center">
+        Copyright 2020 <br />
+        Pixel Pajama Studios
+      </div>
+      <div></div>
+    </div>
+  );
 }
