c0e7688f06a1692d2aef46f27b6e5a914aad3792
fedellen <fedellen@pixelpajamastudios.com>
2021-01-05 11:09:07 -0600

    Handle displaying modal logic in unique component

---
+++ web/src/components/modals/CurrentModal.tsx
@@ -0,0 +1,33 @@
+import { useStateValue } from 'src/state/state';
+import { ItemOptions } from './ItemOptions';
+import { Menu } from './Menu';
+import Modal from './Modal';
+import SingleInput from './SingleInput';
+
+/** Handles logic for displaying current modal */
+
+export default function CurrentModal() {
+  const [{ modalState }] = useStateValue();
+
+  if (!modalState.active) return null;
+
+  /** Uses `addItem` for default values */
+  let modalTitle = 'Add Item';
+  let component = <SingleInput />;
+
+  if (modalState.type === 'addNote') {
+    modalTitle = 'Add Note';
+    // <SingleInput />
+  } else if (modalState.type === 'createList') {
+    modalTitle = 'Create New List';
+    // <SingleInput />
+  } else if (modalState.type === 'itemOptions') {
+    modalTitle = modalState.itemName || 'options';
+    component = <ItemOptions />;
+  } else if (modalState.type === 'menu') {
+    modalTitle = 'Menu';
+    component = <Menu />;
+  } // else 'addItem'
+
+  return <Modal modalTitle={modalTitle} component={component} />;
+}
