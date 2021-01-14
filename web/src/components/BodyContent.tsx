fac7ac50df942a638f6445fe7541ae8edb6ca5bb
fedellen <fedellen@pixelpajamastudios.com>
2021-01-14 16:20:52 -0600

    Initialize `BodyContent` + `DemoList` components

---
+++ web/src/components/BodyContent.tsx
@@ -0,0 +1,32 @@
+import { useStateValue } from 'src/state/state';
+import CreateUser from './CreateUser';
+import DemoList from './DemoList';
+import HomePage from './HomePage';
+import Login from './Login';
+import UsersLists from './UsersLists';
+
+/** Handles logic for displaying content based on `appState` */
+
+export default function BodyContent() {
+  const [{ appState }] = useStateValue();
+
+  const bodyContent = () => {
+    switch (appState) {
+      case 'list':
+        return <UsersLists />;
+      case 'home':
+        return <HomePage />;
+      case 'login':
+        return <Login />;
+      case 'createUser':
+        return <CreateUser />;
+      case 'demo':
+        return <DemoList />;
+      default:
+        console.error('No body content found for `appState`:', appState);
+        return null;
+    }
+  };
+
+  return <div className="bg-dark">{bodyContent()}</div>;
+}
---
+++ web/src/components/DemoList.tsx
@@ -0,0 +1,3 @@
+export default function DemoList() {
+  return <div>The demo list</div>;
+}
