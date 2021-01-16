a660ee35edbc3a41871a7821e21a99244672678e
fedellen <fedellen@pixelpajamastudios.com>
2021-01-15 20:18:56 -0600

    Move `SideMenu` behind modal overlay

--- web/src/components/SideMenu.tsx
+++ web/src/components/SideMenu.tsx
@@ -2,7 +2,7 @@ import AddItemIcon from './svg/AddItemIcon';
 
 export default function SideMenu() {
   return (
-    <div className="fixed right-6 bottom-6">
+    <div className="fixed right-6 bottom-6 z-10">
       <AddItemIcon />
     </div>
   );
