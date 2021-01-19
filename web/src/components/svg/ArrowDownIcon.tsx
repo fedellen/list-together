d90de67f21565d36cc3c62e2eb7163220abbd4df
fedellen <fedellen@pixelpajamastudios.com>
2021-01-19 17:24:52 -0600

    Remove color from icons

--- web/src/components/svg/ArrowDownIcon.tsx
+++ web/src/components/svg/ArrowDownIcon.tsx
@@ -6,7 +6,6 @@ export default function ArrowDownIcon() {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
       fill="currentColor"
-      color="#000000"
     >
       <path d="M11.646 15.146L5.854 9.354a.5.5 0 01.353-.854h11.586a.5.5 0 01.353.854l-5.793 5.792a.5.5 0 01-.707 0z"></path>
     </svg>
