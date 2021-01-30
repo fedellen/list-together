089cce7dcc55b06aa71268a7482f719bff62f53f
fedellen <fedellen@pixelpajamastudios.com>
2021-01-29 20:03:28 -0600

    Remove typo

--- web/src/components/BodyContent.tsx
+++ web/src/components/BodyContent.tsx
@@ -5,7 +5,6 @@ import HomePage from './HomePage';
 import Login from './Login';
 import UsersLists from './list/UsersLists';
 
-UsersLists;
 /** Handles logic for displaying content based on `appState` */
 
 export default function BodyContent() {
