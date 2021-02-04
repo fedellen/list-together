626b9794ca65170809cbeb79a2c876605aa324dd
fedellen <fedellen@pixelpajamastudios.com>
2021-02-04 14:00:59 -0600

    Add social media widget with icons

---
+++ web/src/components/styled/SocialIconContainer.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+
+type SocialIconContainerProps = {
+  /** SVG Icon to put display */
+  icon: ReactNode;
+  /** <a href="{link}"> */
+  link: string;
+};
+
+export default function SocialIconContainer({
+  icon,
+  link
+}: SocialIconContainerProps) {
+  return (
+    <a
+      href={link}
+      className="w-10 transform hover:scale-125 transition-all duration-500 hover:text-ppjsOrange"
+    >
+      {icon}
+    </a>
+  );
+}
