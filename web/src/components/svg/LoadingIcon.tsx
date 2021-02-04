118df919cf245b05ae8b77e183ba8a7b16acaf11
fedellen <fedellen@pixelpajamastudios.com>
2021-02-04 14:08:13 -0600

    Add loading icon and ppjs logo

---
+++ web/src/components/svg/LoadingIcon.tsx
@@ -0,0 +1,101 @@
+export default function LoadingIcon() {
+  // Loading icon from https://svgbox.net/iconset/loaders
+  return (
+    <svg
+      width="32px"
+      height="32px"
+      viewBox="0 0 58 58"
+      xmlns="http://www.w3.org/2000/svg"
+      color="#000000"
+    >
+      <g
+        stroke="currentColor"
+        fill="currentColor"
+        transform="translate(2 1)"
+        strokeWidth="1.5"
+        fillRule="evenodd"
+      >
+        <circle cx="42.601" cy="11.462" r="5">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="1;0;0;0;0;0;0;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          ></animate>
+        </circle>
+        <circle cx="49.063" cy="27.063" r="5">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;1;0;0;0;0;0;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          ></animate>
+        </circle>
+        <circle cx="42.601" cy="42.663" r="5">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;0;1;0;0;0;0;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          ></animate>
+        </circle>
+        <circle cx="27" cy="49.125" r="5">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;0;0;1;0;0;0;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          ></animate>
+        </circle>
+        <circle cx="11.399" cy="42.663" r="5">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;0;0;0;1;0;0;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          ></animate>
+        </circle>
+        <circle cx="4.938" cy="27.063" r="5">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;0;0;0;0;1;0;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          ></animate>
+        </circle>
+        <circle cx="11.399" cy="11.462" r="5">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;0;0;0;0;0;1;0"
+            calcMode="linear"
+            repeatCount="indefinite"
+          ></animate>
+        </circle>
+        <circle cx="27" cy="5" r="5">
+          <animate
+            attributeName="fill-opacity"
+            begin="0s"
+            dur="1.3s"
+            values="0;0;0;0;0;0;0;1"
+            calcMode="linear"
+            repeatCount="indefinite"
+          ></animate>
+        </circle>
+      </g>
+    </svg>
+  );
+}
