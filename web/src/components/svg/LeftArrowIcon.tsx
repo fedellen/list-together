0f46f26fe7a6905d2c794b099d395f2a2f4fe08d
fedellen <fedellen@pixelpajamastudios.com>
2021-02-04 14:08:38 -0600

    Remove whitespace from `ArrowIcon`

--- web/src/components/svg/LeftArrowIcon.tsx
+++ web/src/components/svg/LeftArrowIcon.tsx
@@ -1,12 +1,12 @@
 export default function LeftArrowIcon() {
   // Triangle icons from https://svgbox.net/iconset/octicons
+  // Whitespace removal from https://svgcrop.com/
   return (
     <svg
-      width="50px"
-      height="50px"
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="4 0 24 24"
       fill="currentColor"
+      viewBox="8.68 5.68 6.82 12.62"
+      className="static opacity-100"
     >
       <path d="M8.854 11.646l5.792-5.792a.5.5 0 01.854.353v11.586a.5.5 0 01-.854.353l-5.792-5.792a.5.5 0 010-.708z"></path>
     </svg>
--- web/src/components/svg/RightArrowIcon.tsx
+++ web/src/components/svg/RightArrowIcon.tsx
@@ -1,12 +1,12 @@
 export default function RightArrowIcon() {
   // Triangle icons from https://svgbox.net/iconset/octicons
+  // Whitespace removal from https://svgcrop.com/
   return (
     <svg
-      width="50px"
-      height="50px"
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 16 24"
       fill="currentColor"
+      viewBox="8.5 5.68 6.82 12.62"
+      className="static opacity-100"
     >
       <path d="M15.146 12.354l-5.792 5.792a.5.5 0 01-.854-.353V6.207a.5.5 0 01.854-.353l5.792 5.792a.5.5 0 010 .708z"></path>
     </svg>
