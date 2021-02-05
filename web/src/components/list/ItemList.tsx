87bd4d69bfe87cc6ddd791cf887c9be4a657a420
fedellen <fedellen@pixelpajamastudios.com>
2021-02-05 11:19:46 -0600

    Add `SideMenu` inside ItemList for `position: sticky`

--- web/src/components/list/ItemList.tsx
+++ web/src/components/list/ItemList.tsx
@@ -1,4 +1,5 @@
 import { List } from 'src/generated/graphql';
+import SideMenu from '../SideMenu';
 import SingleItem from './SingleItem';
 
 type ItemListProps = {
@@ -8,25 +9,26 @@ type ItemListProps = {
 };
 
 export default function ItemList({ list, sortedItems }: ItemListProps) {
-  if (!list.items || list.items.length === 0) {
-    return (
-      <div className="flex justify-center items-center py-6">
-        <div className="text-xl italic">This list is empty ✍🏾</div>
-      </div>
-    );
-  }
   /** Sort the item data based on User's sorted preferences */
-  const orderedItemsToDisplay = list.items.map((itemArray) => itemArray);
-  orderedItemsToDisplay.sort((a, b) => {
+  const orderedItemsToDisplay = list.items?.map((itemArray) => itemArray);
+  orderedItemsToDisplay?.sort((a, b) => {
     return sortedItems.indexOf(a.name) - sortedItems.indexOf(b.name);
   });
 
   return (
-    // <div className="container mx-auto">
-    <ul className="my-4 rounded-md pl-12 pr-2 py-4 bg-gray-200 shadow-md">
-      {orderedItemsToDisplay.map((i) => (
-        <SingleItem item={i} key={i.name} />
-      ))}
-    </ul>
+    <div className="my-4 pt-4 bg-gray-200 shadow-md flex-col flex rounded-lg">
+      <ul className="px-8">
+        {list.items && list.items.length > 0 ? (
+          orderedItemsToDisplay?.map((i) => (
+            <SingleItem item={i} key={i.name} />
+          ))
+        ) : (
+          <div className="italic text-2xl text-center pb-4">
+            This list is empty ✍🏾
+          </div>
+        )}
+      </ul>
+      <SideMenu />
+    </div>
   );
 }
