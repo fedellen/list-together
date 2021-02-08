89b4eaf50a64a59668a37de89bef9974ec39d174
fedellen <fedellen@pixelpajamastudios.com>
2021-02-08 13:05:54 -0600

    Add `password` type for confirmation field

--- web/src/components/form/CreateUser.tsx
+++ web/src/components/form/CreateUser.tsx
@@ -73,6 +73,7 @@ export default function CreateUser() {
             <FormikTextInput
               name="passwordConfirmation"
               placeholder="confirm password"
+              type="password"
             />
 
             <div className="pt-2">
