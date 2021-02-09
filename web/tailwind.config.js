8831fbb09325eb118509b43228d2ac39d5e30b33
fedellen <fedellen@pixelpajamastudios.com>
2021-02-09 12:47:14 -0600

    Lower `sm:` breakpoint for smallest designs

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -2,25 +2,23 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    // colors: {
-    //   // // Purple Theme
-    //   // lighter: '#95bdc4',
-    //   // light: '#313457',
-    //   // medium: '#120a1a',
-    //   // dark: '#120a1a',
-    //   // darker: '#07020a'
-    //   // lighter: '#d8d8f6', // Lavender Web
-    //   // light: '#B18FCF', // African Violet
-    //   // medium: '#978897', // Hellotrope Gray
-    //   // dark: '#494850', // Dark liver
-    //   // darker: '#2C2C34' // Raisen Black
-    // },
+    screens: {
+      sm: '460px',
+
+      md: '670px',
+
+      lg: '1024px',
+
+      xl: '1280px',
+
+      '2xl': '1536px'
+    },
     container: {
       center: true,
       padding: {
-        DEFAULT: '1.5rem',
-        sm: '2.5rem',
-        md: '5rem',
+        DEFAULT: '0.75rem',
+        sm: '1.25rem',
+        md: '4rem',
         lg: '7rem',
         xl: '9rem',
         '2xl': '12rem'
