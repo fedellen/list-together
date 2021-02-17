48f797b28a3e1180bfae93e24353c3c672b52f51
fedellen <fedellen@pixelpajamastudios.com>
2021-02-17 16:37:59 -0600

    Add `currentSortedItems` fragment hook

---
+++ web/src/hooks/fragmentHooks/useCurrentSortedItems.ts
@@ -0,0 +1,19 @@
+import { useApolloClient, gql } from '@apollo/client';
+import { UserToList } from 'src/generated/graphql';
+import { useStateValue } from 'src/state/state';
+
+export default function useCurrentSortedItems(): string[] | undefined | null {
+  const apolloClient = useApolloClient();
+  const [{ currentListId }] = useStateValue();
+
+  const userListFrag: UserToList | null = apolloClient.readFragment({
+    id: `UserToList:{"listId":"${currentListId}"}`,
+    fragment: gql`
+      fragment CurrentSortedItems on UserToList {
+        sortedItems
+      }
+    `
+  });
+
+  return userListFrag ? userListFrag.sortedItems : null;
+}
