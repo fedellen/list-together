7b052df252f8344501958d0d480bf4d1d778f075
fedellen <fedellen@pixelpajamastudios.com>
2021-02-24 15:37:58 -0600

    Add global rate limit to backend

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,8 +6,27 @@ import { sessionMiddleware } from './middleware/session';
 import { twitterStrategy } from './utils/twitterStrategy';
 import { facebookStrategy } from './utils/facebookStrategy';
 
+import RateLimit from 'express-rate-limit';
+
+import RedisStore from 'rate-limit-redis';
+import { redis } from './redis';
+
 /** Express app */
 const app = express();
+
+// // Use 'trust proxy' when behind nginx etc.
+// app.set('trust proxy', 1);
+
+// Add global rate limit in case malicious spam from IP
+app.use(
+  RateLimit({
+    store: new RedisStore({
+      client: redis
+    }),
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 1000 // limit each IP to 1000 requests per windowMs
+  })
+);
 
 app.use(sessionMiddleware);
 
@@ -18,8 +37,5 @@ passport.use(facebookStrategy);
 
 app.use(passport.initialize());
 app.use('/auth', oAuthRouter);
-
-// // Use 'trust proxy' when behind nginx etc.
-// app.set('trust proxy', 1);
 
 export default app;
