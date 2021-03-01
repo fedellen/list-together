6f1289aabdc6506ac033ff84f4f0bf9ae082c83f
fedellen <fedellen@pixelpajamastudios.com>
2021-02-28 20:11:14 -0600

    Refactor `Note` logic out and simplify `SingleItem`

--- web/src/components/list/SingleItem.tsx
+++ web/src/components/list/SingleItem.tsx
@@ -1,53 +1,18 @@
-import { Item, useDeleteNoteMutation } from 'src/generated/graphql';
-import useCurrentPrivileges from 'src/hooks/fragments/useCurrentPrivileges';
+import { Item } from 'src/generated/graphql';
 import { useStateValue } from 'src/state/state';
-import { errorNotifaction } from 'src/utils/errorNotification';
-import DeleteIcon from '../svg/itemOptions/DeleteIcon';
 import { ItemOptions } from './ItemOptions';
+import Note from './Note';
 
 type SingleItemProps = {
   item: Item;
 };
 
 export default function SingleItem({ item }: SingleItemProps) {
-  const [{ currentListId, activeItem, activeNote }, dispatch] = useStateValue();
-  const [deleteNote, { loading: deleteNoteLoading }] = useDeleteNoteMutation();
-
-  const currentPrivileges = useCurrentPrivileges();
-
-  const userCanDeleteNotes: boolean =
-    currentPrivileges === 'owner' || currentPrivileges === 'delete';
-
-  const handleDeleteNote = async (note: string) => {
-    if (!deleteNoteLoading) {
-      try {
-        /** Use `deleteNote` mutation */
-        const { data } = await deleteNote({
-          variables: {
-            data: {
-              itemName: item.name,
-              listId: currentListId,
-              note: note
-            }
-          }
-        });
-        if (data?.deleteNote.errors) {
-          errorNotifaction(data.deleteNote.errors, dispatch);
-        } else {
-          dispatch({ type: 'SET_ACTIVE_NOTE', payload: ['', ''] });
-        }
-      } catch (err) {
-        console.error(`Error on Delete Note mutation: ${err}`);
-      }
-    }
-  };
+  const [{ activeItem }, dispatch] = useStateValue();
 
   /** activeItem contains item name as string when the user has clicked on an item */
   const isItemActive = activeItem[0] === item.name ? ' active' : '';
   const isStriked = item.strike ? ' strike' : '';
-  const hasActiveNote = activeNote[0] === item.name;
-
-  console.log('`SingleItem` render');
 
   return (
     <li className="item-container">
@@ -63,39 +28,12 @@ export default function SingleItem({ item }: SingleItemProps) {
       {item.notes && (
         <ul>
           {item.notes.map((note) => (
-            <li className="note" key={note}>
-              {/** Note with togglable delete button */}
-              {userCanDeleteNotes ? (
-                <>
-                  <button
-                    className={`note-button${isStriked}${
-                      hasActiveNote && activeNote[1] === note
-                        ? ' hover:text-indigo-700'
-                        : ''
-                    }`}
-                    onClick={() =>
-                      dispatch({
-                        type: 'SET_ACTIVE_NOTE',
-                        payload: [item.name, note]
-                      })
-                    }
-                  >
-                    {note}
-                  </button>
-                  {hasActiveNote && activeNote[1] === note && (
-                    <button
-                      className="delete-note"
-                      onClick={() => handleDeleteNote(note)}
-                    >
-                      <DeleteIcon />
-                    </button>
-                  )}
-                </>
-              ) : (
-                /** Note without togglable delete button */
-                <span className={`note-button${isStriked}`}>{note}</span>
-              )}
-            </li>
+            <Note
+              item={item.name}
+              key={note}
+              note={note}
+              isStriked={isStriked}
+            />
           ))}
         </ul>
       )}
