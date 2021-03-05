fbb737e7551ae7c359cab865b29bcb7a03f2e3c1
fedellen <fedellen@pixelpajamastudios.com>
2021-03-05 14:27:21 -0600

    Reduce notification size; refactor to tailwind file

--- web/src/components/modals/ErrorNotification.tsx
+++ web/src/components/modals/ErrorNotification.tsx
@@ -5,9 +5,5 @@ import { useStateValue } from 'src/state/state';
 export default function ErrorNotification() {
   const [{ errorMessage }] = useStateValue();
 
-  return (
-    <div className="fixed bottom-6 left-6 p-6 text-lg font-semibold w-72 bg-indigo-300 rounded-lg shadow-md z-50">
-      {errorMessage}
-    </div>
-  );
+  return <div id="notification">{errorMessage}</div>;
 }
