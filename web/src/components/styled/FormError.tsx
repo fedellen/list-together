e899049bde30f19029e2af5c8e25c28b072371e7
fedellen <fedellen@pixelpajamastudios.com>
2021-03-07 22:19:06 -0600

    Remove unused components

--- web/src/components/styled/FormError.tsx
+++
@@ -1,7 +0,0 @@
-type FormErrorProps = {
-  errorMessage: string;
-};
-
-export default function FormError({ errorMessage }: FormErrorProps) {
-  return <span className="text-red-600">{errorMessage}</span>;
-}
