bddd7a104820727168928df150469811cee05782
fedellen <fedellen@pixelpajamastudios.com>
2021-03-12 21:47:57 -0600

    Pass REDIS_URL directly as RedisOptions to connect pubSub on prod

--- server/src/utils/pubSub.ts
+++ server/src/utils/pubSub.ts
@@ -1,11 +1,13 @@
 import { RedisPubSub } from 'graphql-redis-subscriptions';
+import { RedisOptions } from 'ioredis';
 
 /** Redis pubSub from `graphql-redis-subscriptions` */
 export const pubSub = new RedisPubSub({
-  connection: {
-    retryStrategy: (options) => {
-      // reconnect after upto 3000 milis
-      return Math.max(options * 100, 3000);
-    }
-  }
+  connection: process.env.REDIS_URL as RedisOptions
+  // {
+  //   retryStrategy: (options) => {
+  //     // reconnect after upto 3000 milis
+  //     return Math.max(options * 100, 3000);
+  //   }
+  // }
 });
