f57605d2cffc5fa514588b12108f53a87e48f657
fedellen <fedellen@pixelpajamastudios.com>
2021-03-13 12:07:13 -0600

    Add useField hook with initial delay for modals

---
+++ web/src/hooks/useField.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import useDelayedFunction from 'src/hooks/useDelayedFunction';
+
+/** Custom Hook for handling inputs */
+export const useField = () => {
+  const [value, setValue] = useState('');
+  const [initialDelay, setInitialDelay] = useState(true);
+  const triggerDelay = useDelayedFunction(() => setInitialDelay(false));
+
+  useEffect(() => {
+    triggerDelay(100); // .1 second delay
+  }, []);
+
+  const onChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    if (!initialDelay) {
+      setValue(event.target.value);
+    }
+  };
+
+  return {
+    value,
+    onChange
+  };
+};
