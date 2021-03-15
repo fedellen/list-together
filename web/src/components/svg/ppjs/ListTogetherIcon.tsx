ac1d5158e05362fcd4af33f695e81563a8922d20
fedellen <fedellen@pixelpajamastudios.com>
2021-03-15 16:47:51 -0500

    Update List Together icon

--- web/src/components/svg/ppjs/ListTogetherIcon.tsx
+++ web/src/components/svg/ppjs/ListTogetherIcon.tsx
@@ -7,79 +7,60 @@ export default function ListTogetherIcon() {
       viewBox="0 0 164.02 164.02"
     >
       <title>List Together Icon</title>
-      <rect x="4" y="3.92" width="154.48" height="154.48" fill="#d1d5db" />
-      <g opacity="0.56">
-        <rect x="2.22" y="34.18" width="158.3" height="4" fill="#9ca3af" />
+      <g id="Layer_1" data-name="Layer 1">
+        <rect x="4" y="3.92" width="154.48" height="154.48" fill="#d1d5db" />
+        <g opacity="0.56">
+          <rect x="2.22" y="34.18" width="158.3" height="4" fill="#9ca3af" />
+        </g>
+        <g opacity="0.56">
+          <rect x="2.22" y="57.16" width="158.3" height="4" fill="#9ca3af" />
+        </g>
+        <g opacity="0.56">
+          <rect x="2.22" y="82.18" width="158.3" height="4" fill="#9ca3af" />
+        </g>
+        <g opacity="0.56">
+          <rect x="2.22" y="107.2" width="158.3" height="4" fill="#9ca3af" />
+        </g>
+        <g opacity="0.56">
+          <rect x="2.22" y="132.22" width="158.3" height="4" fill="#9ca3af" />
+        </g>
+        <rect x="30.57" y="2.48" width="6.63" height="159.83" fill="#9ca3af" />
+        <path
+          d="M168,181.57H31a13.52,13.52,0,0,1-13.5-13.5v-137A13.51,13.51,0,0,1,31,17.55H168a13.51,13.51,0,0,1,13.5,13.5v137A13.51,13.51,0,0,1,168,181.57ZM31,24.55a6.51,6.51,0,0,0-6.5,6.5v137a6.51,6.51,0,0,0,6.5,6.5H168a6.51,6.51,0,0,0,6.5-6.5v-137a6.5,6.5,0,0,0-6.5-6.5Z"
+          transform="translate(-17.49 -17.55)"
+          fill="#1e3a8a"
+        />
       </g>
-      <g opacity="0.56">
-        <rect x="2.22" y="57.16" width="158.3" height="4" fill="#9ca3af" />
+      <g id="Layer_2" data-name="Layer 2">
+        <polyline
+          points="61.05 40.62 49.27 54.05 44.15 48.21"
+          fill="none"
+          stroke="#1e3a8a"
+          strokeMiterlimit="10"
+          strokeWidth="4"
+        />
+        <polyline
+          points="61.05 64.95 49.27 78.39 44.15 72.55"
+          fill="none"
+          stroke="#1e3a8a"
+          strokeMiterlimit="10"
+          strokeWidth="4"
+        />
+        <polyline
+          points="61.05 89.29 49.27 102.72 44.15 96.88"
+          fill="none"
+          stroke="#1e3a8a"
+          strokeMiterlimit="10"
+          strokeWidth="4"
+        />
+        <polyline
+          points="61.05 113.62 49.27 127.06 44.15 121.22"
+          fill="none"
+          stroke="#1e3a8a"
+          strokeMiterlimit="10"
+          strokeWidth="4"
+        />
       </g>
-      <g opacity="0.56">
-        <rect x="2.22" y="82.18" width="158.3" height="4" fill="#9ca3af" />
-      </g>
-      <g opacity="0.56">
-        <rect x="2.22" y="107.2" width="158.3" height="4" fill="#9ca3af" />
-      </g>
-      <g opacity="0.56">
-        <rect x="2.22" y="132.22" width="158.3" height="4" fill="#9ca3af" />
-      </g>
-      <rect x="30.57" y="2.48" width="6.63" height="159.83" fill="#9ca3af" />
-      <path
-        d="M168,181.57H31a13.52,13.52,0,0,1-13.5-13.5v-137A13.51,13.51,0,0,1,31,17.55H168a13.51,13.51,0,0,1,13.5,13.5v137A13.51,13.51,0,0,1,168,181.57ZM31,24.55a6.51,6.51,0,0,0-6.5,6.5v137a6.51,6.51,0,0,0,6.5,6.5H168a6.51,6.51,0,0,0,6.5-6.5v-137a6.5,6.5,0,0,0-6.5-6.5Z"
-        transform="translate(-17.49 -17.55)"
-        fill="#1e3a8a"
-      />
-      <rect
-        x="87.17"
-        y="30.26"
-        width="23.5"
-        height="21.86"
-        rx="4.32"
-        transform="translate(40.09 122.5) rotate(-89.91)"
-        fill="#1e3a8a"
-      />
-      <rect
-        x="87.94"
-        y="46.11"
-        width="21.85"
-        height="96.32"
-        transform="translate(-17.35 -17.69) rotate(0.09)"
-        fill="#1e3a8a"
-      />
-      <path
-        d="M98.75,169.69l11-27.24-21.84,0Z"
-        transform="translate(-17.49 -17.55)"
-        fill="#9ca3af"
-      />
-      <polygon
-        points="81.26 152.14 84.05 145.2 78.49 145.2 81.26 152.14"
-        fill="#1e3a8a"
-      />
-      <path
-        d="M138.05,169.69l9.52-27.24-19,0Z"
-        transform="translate(-17.49 -17.55)"
-        fill="#9ca3af"
-      />
-      <polygon
-        points="120.59 152.14 122.95 145.17 118.16 145.2 120.59 152.14"
-        fill="#1e3a8a"
-      />
-      <circle cx="51.85" cy="48.54" r="6.38" fill="#1e3a8a" />
-      <circle cx="51.85" cy="72.93" r="6.38" fill="#1e3a8a" />
-      <circle cx="51.85" cy="97.32" r="6.38" fill="#1e3a8a" />
-      <circle cx="51.85" cy="121.71" r="6.38" fill="#1e3a8a" />
-      <rect x="111.12" y="46.98" width="18.96" height="77.89" fill="#9ca3af" />
-      <path
-        d="M145.41,30.81H130.77a2.42,2.42,0,0,0-2.16,2.6v49.3h19V33.41A2.42,2.42,0,0,0,145.41,30.81Z"
-        transform="translate(-17.49 -17.55)"
-        fill="#1e3a8a"
-      />
-      <path
-        d="M151.53,34.7h-1.19a1,1,0,0,0-1,1V67l1,11.87h2.19V35.7A1,1,0,0,0,151.53,34.7Z"
-        transform="translate(-17.49 -17.55)"
-        fill="#1e3a8a"
-      />
-      <rect x="129.43" y="17.15" width="4.01" height="3.45" fill="#1e3a8a" />
     </svg>
   );
 }
