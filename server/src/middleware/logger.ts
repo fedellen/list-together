e3e53cc25ea01a49228603e31e8f9d162ec05ee9
fedellen <fedellen@pixelpajamastudios.com>
2021-03-18 12:16:51 -0500

    Add winston logging 🗞

--- server/src/middleware/logger.ts
+++ server/src/middleware/logger.ts
@@ -1,18 +1,36 @@
 import { MiddlewareFn } from 'type-graphql';
 import { MyContext } from '../MyContext';
+import { createLogger, format, transports } from 'winston';
+
+const { combine, timestamp, json } = format;
+
+const winstonLogger = createLogger({
+  level: 'info',
+  format: combine(timestamp(), json()),
+  transports: [
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({
+      filename: `logs/${new Date().toDateString()}.log`
+    })
+  ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  winstonLogger.add(new transports.Console({ format: format.simple() }));
+}
 
 /** Logging middleware for GraphQL resolvers */
 export const logger: MiddlewareFn<MyContext> = async (
   { args, context, info },
   next
 ) => {
+  /** Don't log resolver info in test */
   if (process.env.NODE_ENV !== 'test') {
-    console.log(
-      `🔥 New GraphQL Request! 🔥 
-    Arguments: ${JSON.stringify(args, null, 4)},
-    UserId: ${context.req.session.userId},
-    ResolverType: ${info.fieldName}`
-    );
+    winstonLogger.info([
+      `ResolverType: ${info.fieldName}`,
+      ` UserId: ${context.req.session.userId}`,
+      ` Arguments: ${JSON.stringify(args, null, 4)}`
+    ]);
   }
 
   return next();
