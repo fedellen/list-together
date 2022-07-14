6d6633b6ab0862c1c0d2fed2e102a34cec9d64ab
fedellen <fedellen@pixelpajamastudios.com>
2022-07-13 20:47:35 -0500

    feat(add item): Always sort new items to front of list

--- server/src/services/item/sortIntoList.ts
+++ server/src/services/item/sortIntoList.ts
@@ -1,44 +1,20 @@
 import { UserToList } from '../../entities';
 
-/** Insert item into sortedItems array of UserToList with `itemHistory` */
+/** Insert item into sortedItems array of UserToList */
 export const sortIntoList = (
-  /** UserToList with `itemHistory` */
   userToList: UserToList,
   itemName: string,
   striked = false
+  // indexOfLastInteraction: number TODO: Use last touch on screen for list add? Consider List Options
 ): string[] => {
   if (!userToList.sortedItems || userToList.sortedItems.length < 1) {
-    // UserToList has no sortedItems array, initialize
-    userToList.sortedItems = [itemName];
-  } else if (striked) {
-    userToList.sortedItems = [...userToList.sortedItems, itemName];
-  } else {
-    // Find item in list
-    const itemInHistory = userToList.itemHistory?.find(
-      (history) => itemName === history.item
-    );
-    if (!itemInHistory?.removalRatingArray) {
-      // Item has no history, insert at the front of current sortedItems
-      userToList.sortedItems = [itemName, ...userToList.sortedItems];
-    } else {
-      // User has removal history for item
-      // Insert item into user's preferred order
-      const itemRating = itemInHistory.removalRating(itemInHistory);
-      const indexToInsert = Math.round(
-        // Convert rating (1-1000) into decimal, then multiply by length to find index
-        userToList.sortedItems.length * (itemRating / 1000)
-      );
-
-      // Advanced filter mechanics incoming
-      const itemsBefore = userToList.sortedItems.filter(
-        (_, index) => index < indexToInsert
-      );
-      const itemsAfter = userToList.sortedItems.filter(
-        (_, index) => index >= indexToInsert
-      );
-
-      userToList.sortedItems = [...itemsBefore, itemName, ...itemsAfter]; // 🔥
-    }
+    // UserToList sortedItems array is null or empty, initialize
+    return [itemName];
   }
-  return userToList.sortedItems;
+
+  if (striked) {
+    return [...userToList.sortedItems, itemName];
+  }
+
+  return [itemName, ...userToList.sortedItems];
 };
