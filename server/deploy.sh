215873b70c19bb2ddd1b91e54c5d9aeba0d83b71
Derek Sonnenberg <fedellen@pixelpajamastudios.com>
2023-03-15 20:15:47 -0600

    chore: add .nvmrc file for current server dockerfile

--- server/deploy.sh
+++ server/deploy.sh
@@ -6,6 +6,6 @@ read VERSION
 echo What is the server ip address?
 read SERVERIP
 
-docker build -t fedellen/list-together:$VERSION .
+docker build --build-arg "NODE_VERSION=$(cat .nvmrc | tr -cd '[:digit:]'.)" -t fedellen/list-together:$VERSION .
 docker push fedellen/list-together:$VERSION
 ssh root@$SERVERIP "docker pull fedellen/list-together:$VERSION && docker tag fedellen/list-together:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
