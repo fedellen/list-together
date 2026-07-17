26da511a3b2a81776761259c39382f0c172c5785
derek r sonnenberg <fedellen@pixelpajamastudios.com>
2026-07-17 15:17:41 -0500

    test: update test setup for clear db errors

--- server/src/test-helpers/setupFile.ts
+++ server/src/test-helpers/setupFile.ts
@@ -1,14 +1,17 @@
+import 'dotenv-safe/config';
 import 'reflect-metadata';
 import { testConn } from './testConn';
 import { Connection } from 'typeorm';
 
 // Use --runInBand to avoid closing connection during tests
 
-let connection: Connection;
+let connection: Connection | undefined;
 beforeAll(async () => {
   connection = await testConn();
 });
 
 afterAll(async () => {
-  await connection.close();
+  if (connection?.isConnected) {
+    await connection.close();
+  }
 });
